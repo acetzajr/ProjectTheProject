f57fe6bd8e36caf5ac4e9794b21cd628be59b7ef
José Martínez <marquevjosman@gmail.com>
2025-01-26 19:52:38 -0600

    up

--- GameTheGame/source/main.c
+++ GameTheGame/source/main.c
@@ -1,20 +1,15 @@
 #include "metadata.h"
 
-#include <SDL3/SDL_error.h>
-#include <SDL3/SDL_init.h>
-#include <SDL3/SDL_rect.h>
-#include <SDL3/SDL_render.h>
-#include <SDL3/SDL_version.h>
-#include <SDL3/SDL_video.h>
-
-#include <cairo/cairo.h>
-
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define clean(f) __attribute__((cleanup(f)))
+#include <SDL3/SDL.h>
+
+#include <cairo/cairo.h>
+
+#define clean(f) __attribute__ ((cleanup (f)))
 
 int surface_width = 360;
 int surface_height = 180;
@@ -22,89 +17,134 @@ int window_width = 1024;
 int window_height = 512;
 
 void
-ejemplo ()
+quit_sdl (bool *sdl_initialized)
 {
-  void clean1 (void *ptr)
-  {
-    printf ("Destroying 1\n");
-  }
-  void clean2 (void *ptr)
-  {
-    printf ("Destroying 2\n");
-  }
-  int clean (clean1) a;
-  int clean (clean2) b;
+  if (*sdl_initialized)
+    {
+      SDL_Quit ();
+      *sdl_initialized = false;
+      printf ("SDL quitted. %s\n", SDL_GetError ());
+    }
+}
+
+void
+destroy_window (SDL_Window **window)
+{
+  if (*window)
+    {
+      SDL_DestroyWindow (*window);
+      *window = nullptr;
+      printf ("window destroyed\n");
+    }
+}
+
+void
+destroy_renderer (SDL_Renderer **renderer)
+{
+  if (*renderer)
+    {
+      SDL_DestroyRenderer (*renderer);
+      renderer = nullptr;
+      printf ("renderer destroyed\n");
+    }
+}
+
+void
+destroy_texture (SDL_Texture **texture)
+{
+  if (*texture)
+    {
+      SDL_DestroyTexture (*texture);
+      texture = nullptr;
+      printf ("texture destroyed\n");
+    }
+}
+
+void
+destroy_surface (cairo_surface_t **surface)
+{
+  cairo_surface_destroy (*surface);
+  *surface = nullptr;
+  printf ("surface destroyed\n");
+}
+
+void
+destroy_context (cairo_t **context)
+{
+  cairo_destroy (*context);
+  *context = nullptr;
+  printf ("context destroyed\n");
 }
 
 int
 main_1 ()
 {
-  void destroy_window (SDL_Window ** window)
-  {
-    if (*window)
-      {
-        SDL_DestroyWindow (*window);
-      }
-    SDL_Quit ();
-  }
+
   printf ("Welcome to GameTheGame\n");
   printf ("%s\n", SDL_GetRevision ());
   if (!set_app_metadata ())
     {
       fprintf (stderr, "!set_app_metadata()\n");
-      goto exit;
+      return 1;
     }
-  if (!SDL_Init (SDL_INIT_VIDEO))
+  bool clean (quit_sdl) sdl_initialized = SDL_Init (SDL_INIT_VIDEO);
+  if (!sdl_initialized)
     {
-      fprintf (stderr, "!SDL_Init(SDL_INIT_VIDEO)\n");
-      goto exit;
+      fprintf (stderr, "!sdl_initialized\n");
+      return 1;
     }
-  SDL_Window *clean (destroy_window) window =
-    SDL_CreateWindow ("GameTheGame", window_width, window_height, 0);
+  SDL_Window *clean (destroy_window) window
+    = SDL_CreateWindow ("GameTheGame", window_width, window_height, 0);
   if (!window)
     {
       fprintf (stderr, "!window\n");
-      goto exit;
+      return 1;
     }
-  SDL_Renderer *renderer = SDL_CreateRenderer (window, nullptr);
+  SDL_Renderer *clean (destroy_renderer) renderer =
+    SDL_CreateRenderer (window, nullptr);
   if (!renderer)
     {
       fprintf (stderr, "!renderer\n");
-      goto exit;
+      return 1;
     }
   if (!SDL_SetRenderVSync (renderer, 2))
     {
       fprintf (stderr, "!SDL_SetRenderVSync(renderer, 2)\n");
-      goto destroy_renderer;
+      return 1;
     }
-  SDL_Texture *texture = SDL_CreateTexture (renderer, SDL_PIXELFORMAT_ARGB32,
-                                            SDL_TEXTUREACCESS_STREAMING,
-                                            surface_width, surface_height);
+  SDL_Texture *clean (destroy_texture) texture =
+    SDL_CreateTexture (renderer, SDL_PIXELFORMAT_ARGB32,
+                       SDL_TEXTUREACCESS_STREAMING, surface_width,
+                       surface_height);
   if (!texture)
     {
       fprintf (stderr, "!texture\n");
-      goto destroy_renderer;
+      return 1;
     }
   if (!SDL_SetTextureScaleMode (texture, SDL_SCALEMODE_NEAREST))
     {
       fprintf (stderr,
                "!SDL_SetTextureScaleMode(texture, SDL_SCALEMODE_NEAREST)\n");
-      goto destroy_texture;
+      return 1;
     }
-  cairo_surface_t *surface =
+  cairo_surface_t *clean (destroy_surface) surface =
     cairo_image_surface_create (CAIRO_FORMAT_ARGB32, surface_width,
                                 surface_height);
   if (cairo_surface_status (surface) != CAIRO_STATUS_SUCCESS)
     {
       fprintf (stderr,
                "cairo_surface_status(surface) != CAIRO_STATUS_SUCCESS\n");
-      goto destroy_surface;
+      fprintf (stderr, "cairo_surface_status -> %s\n",
+               cairo_status_to_string (cairo_surface_status (surface)));
+      return 1;
     }
-  cairo_t *context = cairo_create (surface);
+  cairo_t *clean (destroy_context) context = cairo_create (surface);
   if (cairo_status (context) != CAIRO_STATUS_SUCCESS)
     {
       fprintf (stderr, "cairo_status(context) != CAIRO_STATUS_SUCCESS\n");
-      goto destroy_context;
+      fprintf (stderr, "cairo_status -> %s\n",
+               cairo_status_to_string (cairo_status (context)));
+      return 1;
     }
   SDL_Rect rect = {.x = 0,.y = 0,.w = surface_width,.h = surface_height };
   SDL_FRect srcrect = {.x = 0,.y = 0,.w = surface_width,.h = surface_height };
@@ -118,11 +158,10 @@ main_1 ()
           switch (event.type)
             {
             case SDL_EVENT_WINDOW_CLOSE_REQUESTED:
-              goto exit_game_loop;
+              return 0;
               break;
             }
         }
-      // cairo_set_line_width(context, 0);
       cairo_set_source_rgb (context, 1, 1, 1);
       cairo_rectangle (context, 0, 0, surface_width, surface_height);
       cairo_fill (context);
@@ -134,13 +173,13 @@ main_1 ()
       if (!surface_data)
         {
           fprintf (stderr, "!surface_data\n");
-          goto exit_game_loop;
+          return 1;
         }
       int stride = cairo_image_surface_get_stride (surface);
       if (!stride)
         {
           fprintf (stderr, "!stride\n");
-          goto exit_game_loop;
+          return 1;
         }
       int pitch;
       void *pixels;
@@ -166,27 +205,11 @@ main_1 ()
       SDL_RenderTexture (renderer, texture, &srcrect, &dstrect);
       SDL_RenderPresent (renderer);
     }
-exit_game_loop:
-  fprintf (stderr, "cairo_surface_status -> %s\n",
-           cairo_status_to_string (cairo_surface_status (surface)));
-  fprintf (stderr, "cairo_status -> %s\n",
-           cairo_status_to_string (cairo_status (context)));
-destroy_context:
-  cairo_destroy (context);
-destroy_surface:
-  cairo_surface_destroy (surface);
-destroy_texture:
-  SDL_DestroyTexture (texture);
-destroy_renderer:
-  SDL_DestroyRenderer (renderer);
-exit:
-  fprintf (stderr, "SDL_GetError -> %s\n", SDL_GetError ());
   return 0;
 }
 
 int
 main ()
 {
-  ejemplo ();
   return main_1 ();
 }
